0a3fcf57c2e754e1dcc6360a7eaef936dd968c46
leeeeegun <leeeeegun@naver.com>
2025-08-27 10:18:43 +0900

    Initial commit

---
+++ src/main/java/com/example/webdungeonexplorer/WebDungeonExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdungeonexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDungeonExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDungeonExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdungeonexplorer/WebDungeonExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdungeonexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDungeonExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
